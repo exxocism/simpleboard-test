02637ffc8457305ec613aaefa432036a513a0d4f
Charlie_mac <tchoi.tab@gmail.com>
2021-12-11 21:26:56 +0900

    frontend CRUD - R finished

--- content.js
+++ content.js
@@ -1,100 +1,117 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  const endpoint = "https://boardapi.eax.kr:4500";
 
-    //get parameter from address bar
-    let urlParams = new URLSearchParams(window.location.search);
-    const fetchID = urlParams.get('id');
-    if (fetchID === null) {
-        const article = document.getElementById('article');
+  // check if a user is logged in
+  let fetchURL = `${endpoint}/users/userinfo`;
+  fetch(fetchURL, { method: "GET", credentials: "include" })
+    .then((response) => response.json())
+    .then((response) => {
+      const userName = document.querySelector(".username");
+      const loginOrLogout = document.querySelector(".loginOrLogout");
 
-        const articleName = document.createElement('div');
-        articleName.classList.add('articleName');
-        articleName.textContent = "400 Bad Request, redirecting...";
-        article.appendChild(articleName);
+      userName.innerHTML = `Logged in as <span style="color:blue;font-weight:bold;">${response.name}</span>`;
+      loginOrLogout.innerHTML = `<a href="${endpoint}/users/logout">Logout <i class="fa-solid fa-power-off"></i></a>`;
+    })
+    .catch((error) => {
+      //do nothing
+      console.dir(error);
+    });
 
-        setTimeout(() => {
-            window.location.href = "index.html";
-        }, 3000);
-        return ;
-    }
+  //get parameter from address bar
+  let urlParams = new URLSearchParams(window.location.search);
+  const fetchID = urlParams.get("id");
+  if (fetchID === null) {
+    const article = document.getElementById("article");
 
+    const articleName = document.createElement("div");
+    articleName.classList.add("articleName");
+    articleName.textContent = "400 Bad Request, redirecting...";
+    article.appendChild(articleName);
 
-    //fetch data from api
-    const endpoint = `reference/mockupArticle${fetchID}.json`;
-    fetch(endpoint).then( res => res.json() )
-        .then( ({articleData}) => {
-            const forumTag = document.getElementsByClassName('forumTag')[0];
-            forumTag.textContent = articleData.tag;
-            
-            const article = document.getElementById('article');
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 3000);
+    return;
+  }
 
-            const articleName = document.createElement('div');
-            articleName.classList.add('articleName');
-            articleName.textContent = articleData.name;
-            article.appendChild(articleName);
+  //fetch data from api
+  fetchURL = `${endpoint}/border/view?border_idx=${fetchID}`;
+  // fetchURL = `reference/mockupArticle${fetchID}.json`;
+  fetch(fetchURL)
+    .then((res) => res.json())
+    .then((articleData) => {
+      const forumTag = document.getElementsByClassName("forumTag")[0];
+      forumTag.textContent = articleData.category;
 
-            const tagsAndCommentCount = document.createElement('div');
-            tagsAndCommentCount.classList.add('tagsAndCommentCount');
-                const tag = document.createElement('div');
-                tag.id = 'tag';
-                tag.innerHTML = `<i class="fa-solid fa-hashtag"></i> ${articleData.tag}`;
-                tagsAndCommentCount.appendChild(tag);
+      const article = document.getElementById("article");
 
-                const commentCount = document.createElement('div');
-                commentCount.classList.add('commentCount');
-                commentCount.innerHTML = `<i class="fa-regular fa-comments"></i> ${articleData.comments.length}`;
-                tagsAndCommentCount.appendChild(commentCount);
-            article.appendChild(tagsAndCommentCount);
+      const articleName = document.createElement("div");
+      articleName.classList.add("articleName");
+      articleName.textContent = articleData.subject;
+      article.appendChild(articleName);
 
-            const hr = document.createElement('hr');
-            article.appendChild(hr);
+      const tagsAndCommentCount = document.createElement("div");
+      tagsAndCommentCount.classList.add("tagsAndCommentCount");
+      const tag = document.createElement("div");
+      tag.id = "tag";
+      tag.innerHTML = `<i class="fa-solid fa-hashtag"></i> ${articleData.category}`;
+      tagsAndCommentCount.appendChild(tag);
 
-            const nameAndTime = document.createElement('div');
-            nameAndTime.classList.add('nameAndTime');
-                const name = document.createElement('div');
-                name.id = 'name';
-                name.textContent = articleData.author;
-                nameAndTime.appendChild(name);
-                
-                const time = document.createElement('div');
-                time.id = 'time';
-                time.textContent = articleData.lastModified;
-                nameAndTime.appendChild(time);
-            article.appendChild(nameAndTime);
+      const commentCount = document.createElement("div");
+      commentCount.classList.add("commentCount");
+      commentCount.innerHTML = `<i class="fa-regular fa-comments"></i> ${articleData.reply_count}`;
+      tagsAndCommentCount.appendChild(commentCount);
+      article.appendChild(tagsAndCommentCount);
 
-            const content = document.createElement('div');
-            content.id = 'content';
-            content.textContent = articleData.contents;
-            article.appendChild(content);
-            
-            const hr2 = document.createElement('hr');
-            article.appendChild(hr2);
+      const hr = document.createElement("hr");
+      article.appendChild(hr);
 
-            const comments = document.createElement('div');
-            comments.classList.add('comments');
-                const commentsMetadata = document.createElement('div');
-                commentsMetadata.classList.add('commentsMetadata');
-                commentsMetadata.innerHTML = `<i class="fa-regular fa-comments"></i> <span class="commentCount">${articleData.comments.length}</span> Replies`;
-                comments.appendChild(commentsMetadata);
-            article.appendChild(comments);
+      const nameAndTime = document.createElement("div");
+      nameAndTime.classList.add("nameAndTime");
+      const name = document.createElement("div");
+      name.id = "name";
+      name.textContent = articleData.writer_name;
+      nameAndTime.appendChild(name);
 
-            for( const comment of articleData.comments ) {
-                const commentDiv = document.createElement('div');
-                commentDiv.classList.add('comment');
-                commentDiv.textContent = comment;
-                article.appendChild(commentDiv);
-            }
+      const time = document.createElement("div");
+      time.id = "time";
+      time.textContent = new Date(articleData.modify_date).toLocaleDateString();
+      nameAndTime.appendChild(time);
+      article.appendChild(nameAndTime);
 
-        })
-        .catch( err => {
-            const article = document.getElementById('article');
+      const content = document.createElement("div");
+      content.id = "content";
+      content.textContent = articleData.content;
+      article.appendChild(content);
 
-            const articleName = document.createElement('div');
-            articleName.classList.add('articleName');
-            articleName.textContent = "404 Not Found. redirecting...";
-            article.appendChild(articleName);
+      const hr2 = document.createElement("hr");
+      article.appendChild(hr2);
 
-            setTimeout(() => {
-                window.location.href = "index.html";
-            }, 3000);
-        });
+      const comments = document.createElement("div");
+      comments.classList.add("comments");
+      const commentsMetadata = document.createElement("div");
+      commentsMetadata.classList.add("commentsMetadata");
+      commentsMetadata.innerHTML = `<i class="fa-regular fa-comments"></i> <span class="commentCount">${articleData.reply_count}</span> Replies`;
+      comments.appendChild(commentsMetadata);
+      article.appendChild(comments);
+
+      /* for (const comment of articleData.comments) {
+        const commentDiv = document.createElement("div");
+        commentDiv.classList.add("comment");
+        commentDiv.textContent = comment;
+        article.appendChild(commentDiv);
+      } */
+    })
+    .catch((err) => {
+      const article = document.getElementById("article");
+
+      const articleName = document.createElement("div");
+      articleName.classList.add("articleName");
+      articleName.textContent = "404 Not Found. redirecting...";
+      article.appendChild(articleName);
+
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 3000);
+    });
 });
--- index.js
+++ index.js
@@ -1,54 +1,72 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    // check if a user is authorized
-    
+window.addEventListener("DOMContentLoaded", (event) => {
+  const endpoint = "https://boardapi.eax.kr:4500";
 
-    //fetch data from api
-    const endpoint = 'reference/mockup.json';
-    fetch(endpoint).then( res => res.json())
-        .then( ({boarddata}) => {
-            const threadStart = document.getElementById('board');
-            for( thread of boarddata ){
-                const threadElement = document.createElement('div');
-                threadElement.classList.add('thread');
+  // check if a user is logged in
+  let fetchURL = `${endpoint}/users/userinfo`;
+  fetch(fetchURL, { method: "GET", credentials: 'include' })
+    .then((response) => response.json())
+    .then((response) => {
+      const userName = document.querySelector(".username");
+      const loginOrLogout = document.querySelector(".loginOrLogout");
 
-                const nameTag = document.createElement('div');
-                nameTag.classList.add('nameTag');
+      userName.innerHTML = `Logged in as <span style="color:blue;font-weight:bold;">${response.name}</span>`;
+      loginOrLogout.innerHTML = `<a href="${endpoint}/users/logout">Logout <i class="fa-solid fa-power-off"></i></a>`;
+    })
+    .catch((error) => {
+      //do nothing
+      console.dir(error);
+    });
 
-                    const name = document.createElement('div');
-                    name.classList.add('name');
-                    name.innerHTML = `<a href="content.html?id=${thread.contentID}">${thread.name}</a>`;
-                    nameTag.appendChild(name);
-                    
-                    const tag = document.createElement('div');
-                    tag.classList.add('tag');
-                    tag.textContent = thread.tag;
-                    nameTag.appendChild(tag);
+  //fetch data from api
+  fetchURL = `${endpoint}/border/list`;
+  // fetchURL = "reference/mockup.json";
+  fetch(fetchURL)
+    .then((res) => res.json())
+    // .then(({ boarddata }) => {
+    .then((boarddata) => {
+      const threadStart = document.getElementById("board");
+      for (thread of boarddata) {
+        const threadElement = document.createElement("div");
+        threadElement.classList.add("thread");
 
-                threadElement.appendChild(nameTag);
+        const nameTag = document.createElement("div");
+        nameTag.classList.add("nameTag");
 
-                const authorAndTime = document.createElement('div');
-                authorAndTime.classList.add('authorAndTime');
+        const name = document.createElement("div");
+        name.classList.add("name");
+        name.innerHTML = `<a href="content.html?id=${thread.border_idx}">${thread.subject}</a>`;
+        nameTag.appendChild(name);
 
-                    const author = document.createElement('div');
-                    author.classList.add('author');
-                    author.textContent = thread.author;
-                    authorAndTime.appendChild(author);
+        const tag = document.createElement("div");
+        tag.classList.add("tag");
+        tag.textContent = thread.category;
+        nameTag.appendChild(tag);
 
-                    const time = document.createElement('div');
-                    time.classList.add('time');
-                    time.textContent = thread.lastModified;
-                    authorAndTime.appendChild(time);
+        threadElement.appendChild(nameTag);
 
-                threadElement.appendChild(authorAndTime);
+        const authorAndTime = document.createElement("div");
+        authorAndTime.classList.add("authorAndTime");
 
-                const comments = document.createElement('div');
-                comments.classList.add('comments');
-                comments.textContent = thread.comments.length;
-                threadElement.appendChild(comments);
+        const author = document.createElement("div");
+        author.classList.add("author");
+        author.textContent = thread.writer_name;
+        authorAndTime.appendChild(author);
 
-                threadStart.appendChild(threadElement);
-                const line = document.createElement('hr');
-                threadStart.appendChild(line);
-            }
-        });
+        const time = document.createElement("div");
+        time.classList.add("time");
+        time.textContent = new Date(thread.modify_date).toLocaleDateString();
+        authorAndTime.appendChild(time);
+
+        threadElement.appendChild(authorAndTime);
+
+        const comments = document.createElement("div");
+        comments.classList.add("comments");
+        comments.textContent = thread.reply_count;
+        threadElement.appendChild(comments);
+
+        threadStart.appendChild(threadElement);
+        const line = document.createElement("hr");
+        threadStart.appendChild(line);
+      }
+    });
 });
